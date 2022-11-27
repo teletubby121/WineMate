cbcd9a893e785c45b5b3ac6ab204013783f41f8b
keepw <keepwalking2@hotmail.com>
2022-11-27 21:37:38 +0200

    0937  27

--- app/src/main/java/com/example/winemate/ActivityWindow.kt
+++ app/src/main/java/com/example/winemate/ActivityWindow.kt
@@ -10,8 +10,72 @@ class ActivityWindow : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_window)
 
+        var VARIETY = ""
+        var BODY = ""
+        var ACIDITY = ""
+        var AROMATIC = ""
+        var FRUITY = ""
+        var TANNIN = ""
+
+        fun all() {
+            Constants.VARIETY_NAME = VARIETY
+            Constants.PERCENT_1 = BODY
+            Constants.PERCENT_2 = ACIDITY
+            Constants.PERCENT_3 = AROMATIC
+            Constants.PERCENT_4 = FRUITY
+            Constants.PERCENT_5 = TANNIN
+        }
+
+        fun clear() {
+            Constants.VARIETY_NAME = ""
+            Constants.PERCENT_1 = ""
+            Constants.PERCENT_2 = ""
+            Constants.PERCENT_3 = ""
+            Constants.PERCENT_4 = ""
+            Constants.PERCENT_5 = ""
+        }
+
+
         val ASYRTIKO = findViewById<Button>(R.id.ASYRTIKO)
         ASYRTIKO.setOnClickListener {
+            clear()
+            VARIETY = "ASYRTIKO"
+            BODY = "80"
+            ACIDITY = "77"
+            AROMATIC = "55"
+            FRUITY = "44"
+            TANNIN = "77"
+            all()
+
+            val intent = Intent(this, VarietyWindow::class.java)
+            startActivity(intent)
+        }
+
+        val MALAGOUZIA = findViewById<Button>(R.id.MALAGOUZIA)
+        MALAGOUZIA.setOnClickListener {
+            clear()
+            VARIETY = "MALAGOUZIA"
+            BODY = "34"
+            ACIDITY = "76"
+            AROMATIC = "65"
+            FRUITY = "34"
+            TANNIN = "54"
+            all()
+
+            val intent = Intent(this, VarietyWindow::class.java)
+            startActivity(intent)
+        }
+
+        val MOSXOFILERO = findViewById<Button>(R.id.MOSXOFILERO)
+        MOSXOFILERO.setOnClickListener {
+            clear()
+            VARIETY = "MOSXOFILERO"
+            BODY = "67"
+            ACIDITY = "89"
+            AROMATIC = "67"
+            FRUITY = "45"
+            TANNIN = "23"
+            all()
 
             val intent = Intent(this, VarietyWindow::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/winemate/VarietyWindow.kt
+++ app/src/main/java/com/example/winemate/VarietyWindow.kt
@@ -2,10 +2,30 @@ package com.example.winemate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class VarietyWindow : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_variety_window)
+
+
+        val VarietyName = findViewById<TextView>(R.id.VarietyName)
+        VarietyName.text = Constants.VARIETY_NAME.toString()
+
+        val percent_1 = findViewById<TextView>(R.id.percent_1)
+        percent_1.text = Constants.PERCENT_1.toString()
+        val percent_2 = findViewById<TextView>(R.id.percent_2)
+        percent_2.text = Constants.PERCENT_2.toString()
+        val percent_3 = findViewById<TextView>(R.id.percent_3)
+        percent_3.text = Constants.PERCENT_3.toString()
+        val percent_4 = findViewById<TextView>(R.id.percent_4)
+        percent_4.text = Constants.PERCENT_4.toString()
+        val percent_5 = findViewById<TextView>(R.id.percent_5)
+        percent_5.text = Constants.PERCENT_5.toString()
+
+
+
     }
 }
